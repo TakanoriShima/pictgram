918f0a13ea8ef12d53411287e26aa85a5a4bafbc
EC2 Default User <ec2-user@ip-172-31-40-36.ec2.internal>
2022-10-13 10:21:43 +0000

    Rails 04 User新規登録に潜む問題を解決していく

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_13_083542) do
+ActiveRecord::Schema.define(version: 2022_10_13_101026) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
