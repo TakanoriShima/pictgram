9d672bbd25684bc7e8ff717cecf1b4746dbc30dc
EC2 Default User <ec2-user@ip-172-31-44-56.ap-northeast-1.compute.internal>
2023-04-01 11:08:54 +0000

    mod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,10 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  include SessionsHelper
+  # protect_from_forgery with: :exception
 
-  add_flash_types :success, :info, :warning, :danger  
+  # add_flash_types :success, :info, :warning, :danger  
   
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
 
-  def logged_in?
-    !current_user.nil?
-  end
   
-  helper_method :current_user, :logged_in?
+  # helper_method :current_user, :logged_in?
 end
